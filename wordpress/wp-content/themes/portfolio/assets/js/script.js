b355914ad6f02fb26c7d41090a2ab7ea15e835eb
RumoloEttouatiKevin <kevin.ettouati@gmail.com>
2022-04-28 23:59:10 +0200

    Fix : skill cloud for phone

--- wordpress/wp-content/themes/portfolio/assets/js/script.js
+++ wordpress/wp-content/themes/portfolio/assets/js/script.js
@@ -90,9 +90,26 @@ if (document.getElementById('myCanvas')) {
                 activeCursor: 'default',
                 wheelZoom: false,
                 shadowBlur: 2,
+                noMouse: isMobileDevice()
             });
         } catch(e) {
             document.getElementById('myCanvasContainer').style.display = 'none';
         }
     }; 
+}
+
+function isMobileDevice() { 
+    if (
+       navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+    ) {
+        return true;
+    } else {
+        return false;
+    }
 }
