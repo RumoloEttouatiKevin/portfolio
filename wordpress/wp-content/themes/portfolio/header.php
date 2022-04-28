d1dbaea7ea1849348743fe8dbdeaa7350a32a35e
RumoloEttouatiKevin <kevin.ettouati@gmail.com>
2022-04-28 22:51:28 +0200

    Fix : RWD

--- wordpress/wp-content/themes/portfolio/header.php
+++ wordpress/wp-content/themes/portfolio/header.php
@@ -2,6 +2,7 @@
 <html <?= language_attributes() ?>>
     <head>
         <meta charset="<?= bloginfo('charset') ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <?php wp_head() ?>
     </head>
     <body>
